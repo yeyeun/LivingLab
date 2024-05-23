5deec323106c1055f6c1e0c92ce51e4bde623643
young eun <woyelove@gmail.com>
2024-05-23 14:32:32 +0900

    원본 파일로 복구

--- front/lab/src/App.js
+++ front/lab/src/App.js
@@ -9,9 +9,6 @@ function App() {
         <p>
         Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>
-          TEST 최영은
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
