063eac0ab86b89cb315827a990795a7f0c4384b9
young eun <woyelove@gmail.com>
2024-05-23 10:58:22 +0900

    commit Test

--- front/lab/src/App.js
+++ front/lab/src/App.js
@@ -1,9 +1,28 @@
+import logo from "./logo.svg";
 import "./App.css";
-import { RouterProvider } from "react-router-dom";
-import root from "./router/root";
 
 function App() {
-  return <RouterProvider router={root} />;
+  return(
+  <div className="App">
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <p>
+          TEST 최영은
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  )
 }
 
 export default App;
