819c117090b643abd2626d9dc857ff4d6362179c
lwjddns <jwlee20541@gmail.com>
2024-05-23 11:07:46 +0900

    jungwoons first commit

--- front/lab/src/App.js
+++ front/lab/src/App.js
@@ -1,9 +1,24 @@
+import logo from "./logo.svg";
 import "./App.css";
-import { RouterProvider } from "react-router-dom";
-import root from "./router/root";
-
 function App() {
-  return <RouterProvider router={root} />;
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+          깃허브 작업시작(정운)
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
-
 export default App;
