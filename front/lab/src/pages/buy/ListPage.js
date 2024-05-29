ff0b6585b816c593cfbfbe6cf4cd2d6e9ff09721
young eun <woyelove@gmail.com>
2024-05-29 16:15:09 +0900

    [FE] Feat : 정렬 필터 컴포넌트 생성

--- front/lab/src/pages/buy/ListPage.js
+++ front/lab/src/pages/buy/ListPage.js
@@ -1,5 +1,7 @@
 import ListComponent from '../../components/common/ListComponent';
+import LocationComponent from '../../components/common/LocationComponent';
 import SearchComponent from '../../components/common/SearchComponent';
+import SelectComponent from '../../components/common/SelectComponent';
 
 const ListPage = () => {
   return (
@@ -7,8 +9,12 @@ const ListPage = () => {
     <div className="m-auto w-2/5 pb-2 text-3xl font-Jua">
       공동구매
     </div>
+    <div className="m-auto w-2/5 items-center">
+      <LocationComponent/>
+    </div>
     <div className="m-auto bg-slate-200 w-2/5 rounded-md px-10 py-4">
       <SearchComponent/>
+      <SelectComponent/>
       <ListComponent />
       <ListComponent />
       <ListComponent />
--- front/lab/src/pages/team/ListPage.js
+++ front/lab/src/pages/team/ListPage.js
@@ -1,5 +1,7 @@
 import ListComponent from '../../components/common/ListComponent';
 import SearchComponent from '../../components/common/SearchComponent';
+import LocationComponent from '../../components/common/LocationComponent';
+import SelectComponent from '../../components/common/SelectComponent';
 
 const ListPage = () => {
   return (
@@ -7,8 +9,12 @@ const ListPage = () => {
     <div className="m-auto w-2/5 pb-2 text-3xl font-Jua">
       동네모임
     </div>
+    <div className="m-auto w-2/5 items-center">
+      <LocationComponent/>
+    </div>
     <div className="m-auto bg-slate-200 w-2/5 rounded-md px-10 py-4">
       <SearchComponent/>
+      <SelectComponent/>
       <ListComponent />
       <ListComponent />
       <ListComponent />
