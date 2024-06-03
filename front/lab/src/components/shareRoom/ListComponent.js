73d011d482a50be682ae46038740c1c616dd9d82
Wu Sang <a4042718@gmail.com>
2024-06-03 09:28:05 +0900

    [BE] Feat: 자취방쉐어 백엔드 연동 오류상태

--- front/lab/src/components/shareRoom/ListComponent.js
+++ front/lab/src/components/shareRoom/ListComponent.js
@@ -1,67 +1,58 @@
-import image from '../../resources/images/defaultImage.jpg';
-import userIcon from '../../resources/images/user.png';
-import mapIcon from '../../resources/images/map.png';
+import image from '../../resources/images/room.jpg';
 import { useEffect, useState } from "react";
-import { getList } from "../../api/marketApi"
+import { getList } from "../../api/shareRoomApi"
 import useCustomMove from "../../hooks/useCustomMove";
 import PageComponent from "../common/PageComponent";
+import SearchComponent from '../../components/common/SearchComponent';
+
 
 const initState = {
-    dtoList: [], //한 페이지에 불러오는 게시물 갯수
-    pageNumList: [],
-    pageRequestDto: null,
-    prev: false,
-    next: false,
-    totalCount: 0,
-    prevPage: 0,
-    nextPage: 0,
-    totalPage: 0,
-    current: 0
+  dtoList: [], //한 페이지에 불러오는 게시물 갯수
+  pageNumList: [],
+  pageRequestDto: null,
+  prev: false,
+  next: false,
+  totalCount: 0,
+  prevPage: 0,
+  nextPage: 0,
+  totalPage: 0,
+  current: 0
 }
+
 const ListComponent = () => {
-    const{page, size, moveToList, moveToRead} = useCustomMove();
-    const [serverData, setServerData] = useState(initState);
-    useEffect(()=>{
-        getList({page,size}).then(data=>{
-            console.log(data);
-            setServerData(data);
-        })
-    }, [page,size]);
-    return(
-        <div>
-        {serverData.dtoList.map(market=>
-            <div key={market.marketNo} className="w-full mb-4" onClick={()=>moveToRead(market.marketNo)}>
-                <div className="flex flex-col justify-center items-center px-5 bg-white border border-gray-200 rounded-lg shadow sm:flex-row hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700">
-                    <img className="object-cover w-full h-96 md:h-auto md:w-48 md:rounded-none" src={image} alt="..."/>
-                    <div className="flex flex-col p-4 mx-5 leading-normal">
-                        <div className="mb-2 inline-flex">
-                            <span className="bg-white text-gray-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded-xl border border-gray-900 dark:bg-gray-700 dark:text-gray-300">
-                                {market.marketCategory === '1' && '구매'}
-                                {market.marketCategory === '2' && '판매'}
-                                {market.marketCategory === '3' && '교환'}
-                                {market.marketCategory === '4' && '나눔'}
-                            </span>
-                            <div className="bg-white text-gray-800 text-sm font-medium me-2 px-2.5 py-0.5 rounded-xl border border-gray-900 dark:bg-gray-700 dark:text-gray-300">
-                                <img src={userIcon} alt="..." className="w-3 inline"/>&ensp;{market.current} / {market.max}
-                            </div>
-                            <div className="text-base">
-                                진행 중
-                            </div>
-                        </div>
-                        <h5 className="mb-2 text-lg font-bold tracking-tight text-gray-900 dark:text-white">{market.title}</h5>
-                        <div className="mb-3 text-base text-gray-700 dark:text-gray-400">
-                            <img src={mapIcon} alt="..." className="w-3 inline"/>&ensp;{market.location}
-                        </div>
-                        <div className="mb-3 text-base text-gray-700 dark:text-gray-400">
-                            <div className="float-right">{market.user_id}</div>
-                        </div>
-                    </div>
-                </div>
+  const { page, size, moveToList, moveToRead } = useCustomMove();
+  const [serverData, setServerData] = useState(initState);
+  useEffect(() => {
+    getList({ page, size }).then(data => {
+      console.log(data);
+      setServerData(data);
+    })
+  }, [page, size]);
+  return (
+    <div>
+      <SearchComponent />
+      <div class="grid grid-cols-1 gap-12 md:grid-cols-2 xl:grid-cols-3 py-10">
+        {serverData.dtoList.map((shareRoom) =>
+          <div class="mx-12 overflow-hidden rounded-lg shadow-lg cursor-pointer h-90 w-60 md:w-80">
+            <div className="block w-full h-full" onClick={() => moveToRead(shareRoom.roomNo)}>
+              <img alt="..." src={image} class="object-cover w-full max-h-40" />
+              <div class="w-full p-4 bg-white">
+                <p class="font-medium text-indigo-500 text-md">
+                </p>
+                <p class="mb-2 text-xl font-medium text-gray-800">
+                  13평 원룸 2층입니다.
+                </p>
+                <p class="font-light text-gray-400 text-md">
+                  140,000원/월
+                </p>
+              </div>
             </div>
+          </div>
         )}
-        <PageComponent serverData={serverData} movePage={moveToList}/>
-        </div>
-    );
+      </div>
+      <PageComponent serverData={serverData} movePage={moveToList} />
+    </div>
+  );
 }
 
 export default ListComponent;
--- front/lab/src/pages/shareRoom/ListPage.js
+++ front/lab/src/pages/shareRoom/ListPage.js
@@ -1,30 +1,14 @@
-import RoomComponent from '../../components/common/RoomComponent';
-import RoomComponent2 from '../../components/common/RoomComponent2';
-import SearchComponent from '../../components/common/SearchComponent';
+import ListComponent from '../../components/shareRoom/ListComponent';
+
 const ListPage = () => {
   return (
     <>
-    <div className="m-auto w-2/5 pb-2 text-3xl font-Jua">
-      자취방쉐어
-    </div>
-    <div className="m-auto w-3/5 rounded-md px-10 py-4">
-      <SearchComponent/>
-      <div class="grid grid-cols-1 gap-12 md:grid-cols-2 xl:grid-cols-3 py-10">
-        <RoomComponent />
-        <RoomComponent />
-        <RoomComponent />
+      <div className="m-auto w-2/5 pb-2 text-3xl font-Jua">
+        자취방쉐어
       </div>
       <div class="grid grid-cols-1 gap-12 md:grid-cols-2 xl:grid-cols-3 py-10">
-        <RoomComponent2 />
-        <RoomComponent2 />
-        <RoomComponent2 />
+        <ListComponent/>
       </div>
-      <div class="grid grid-cols-1 gap-12 md:grid-cols-2 xl:grid-cols-3 py-10">
-        <RoomComponent />
-        <RoomComponent />
-        <RoomComponent />
-      </div>
-    </div>
     </>
   );
 };
