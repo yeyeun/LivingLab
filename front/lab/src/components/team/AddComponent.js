06f123e2f604b84830f1b64065ed5f935890fda5
young eun <woyelove@gmail.com>
2024-06-12 01:40:55 +0900

    commit

--- front/lab/src/components/team/AddComponent.js
+++ front/lab/src/components/team/AddComponent.js
@@ -1,9 +1,9 @@
 import React, { useState, useEffect, useRef } from 'react';
 import useCustomMove from '../../hooks/useCustomMove';
-import { postAddBuy } from '../../api/buyApi';
+import { postAddTeam } from '../../api/teamApi';
 import ResultModal from '../common/ResultModal';
 import PostComponent from '../common/PostComponent';
-import { modifyUser, getUser } from './../../api/userApi';
+import { getUser } from './../../api/userApi';
 import { useSelector } from 'react-redux';
 import iconNext from '../../resources/images/icon-next.png';
 import iconEdit from '../../resources/images/iconEdit.png'
@@ -14,17 +14,17 @@ const initState = {
   title: '',
   location: '',
   content: '',
-  buyCategory: '',
+  teamCategory: '',
   max: 2,
   current: 1,
   deadline: '',
-  buyHit: 0,
+  teamHit: 0,
   files: [],
 };
 const AddComponent = () => {
   const { moveToList } = useCustomMove();
   const [postImageFiles, setPostImageFiles] = useState([]); // 이미지 파일 프리뷰
-  const [buy, setBuy] = useState({ ...initState });
+  const [team, setTeam] = useState({ ...initState });
   const [result, setResult] = useState(null);
   const [addResultModal, setAddResultModal] = useState(null);
   const imgRef = useRef();
@@ -76,21 +76,29 @@ const AddComponent = () => {
     imgRef.current.files = dataTransfer.files; //제거된 파일 반영
   };
 
-  const handleChangeBuy = (e) => {
+  const handleChangeTeam = (e) => {
     const { name, value } = e.target;
-    setBuy((prev) => ({ ...prev, [name]: value }));
+    setTeam((prev) => ({ ...prev, [name]: value }));
   };
   const handleClickAdd = async () => {
-    if (!buy.buyCategory) {
+    if (!team.teamCategory) {
       setAddResultModal('카테고리를 선택해주세요');
       return;
     }
-    if (!buy.title || !buy.content) {
+    if (!team.title || !team.content) {
       setAddResultModal('제목과 내용을 입력해주세요');
       return;
     }
-    if (!postImageFiles) {
-      setAddResultModal('이미지를 등록해주세요');
+    if(!team.deadline || !team.location){
+      setAddResultModal('마감시간과 모임장소를 입력해주세요');
+      return;
+    }
+    const time = new Date();
+    const timeElement = new Date(team.deadline);
+    console.log("time:",time);
+    console.log("timeElement:",timeElement);
+    if(time > timeElement){
+      setAddResultModal('현재 시간보다 이전의 날짜는 설정할 수 없습니다');
       return;
     }
     const formData = new FormData();
@@ -99,16 +107,20 @@ const AddComponent = () => {
     }
     formData.append('user_id', user.email);
     formData.append('nickname', user.nickname);
-    formData.append('title', buy.title);
-    formData.append('location', buy.location);
-    formData.append('content', buy.content);
-    formData.append('buyCategory', buy.buyCategory);
-    formData.append('max', buy.max);
-    formData.append('current', buy.current);
-    formData.append('deadline', buy.deadline);
-    formData.append('buyHit', buy.buyHit);
-    postAddBuy(formData);
-    setResult('게시글이 등록되었습니다');
+    formData.append('title', team.title);
+    formData.append('location', team.location);
+    formData.append('content', team.content);
+    formData.append('teamCategory', team.teamCategory);
+    formData.append('max', team.max);
+    formData.append('current', team.current);
+    formData.append('deadline', team.deadline);
+    formData.append('teamHit', team.teamHit);
+
+    for (const x of formData.entries()) {
+      console.log(x);
+     };
+    //postAddTeam(formData);
+    //setResult('게시글이 등록되었습니다');
   };
   const closeModal = () => {
     setResult(null);
@@ -116,7 +128,7 @@ const AddComponent = () => {
   };
 
   const setAddress = (address) => {
-    setBuy((prev) => ({ ...prev, location: address }));
+    setTeam((prev) => ({ ...prev, location: address }));
   };
 
   const handleInputValidation = (e) => {
@@ -124,16 +136,16 @@ const AddComponent = () => {
     if (value !== "" && (isNaN(value) || value < 2)) {
       e.target.value = Math.max(2, value);
     }
-    handleChangeBuy(e);
+    handleChangeTeam(e);
   };
 
   return (
     <div>
       <div className="flex items-center w-1/2 mx-auto text-xl font-semibold pl-2 border-l-4 border-teal-300">
-        공동구매 <img src={iconNext} className="w-7 mx-2" alt="Next Icon" /> 글 작성
+        동네모임 <img src={iconNext} className="w-7 mx-2" alt="Next Icon" /> 글 작성
       </div>
       <div className="grid grid-cols-8 gap-3 w-1/2 mx-auto mt-2 p-2 text-xl shadow-set mb-5">
-        <div className="col-start-3 col-span-4 mt-2 border rounded overflow-x-scroll whitespace-nowrap scrollbar-thin scrollbar-thumb-gray-400 scrollbar-track-gray-200">
+        <div className="col-start-3 col-span-4 mt-2 border rounded overflow-x-scroll whitespace-nowrap">
           {postImageFiles.map((file, index) => (
           <div key={index} className="relative m-3 w-36 h-36 inline-block align-top border">
             <img src={file.url} alt={file.name} className="w-36 h-36 object-cover"/>
@@ -154,46 +166,46 @@ const AddComponent = () => {
           </div>
         </div>
         <div className="col-start-2 col-span-2">
-          <label htmlFor="buyCategory" className="flex items-center text-sm font-medium text-gray-700 mb-1">
-            <img src={iconEdit} className="w-3 h-3"></img>&nbsp;카테고리
+          <label htmlFor="teamCategory" className="flex items-center text-sm font-medium text-gray-700 mb-1">
+            <img src={iconEdit} className="w-3 h-3" alt="edit"></img>&nbsp;카테고리
           </label>
-          <select id="buyCategory" name="buyCategory" 
+          <select id="teamCategory" name="teamCategory" 
             className="w-full pl-2 h-9 rounded-md border border-stone-400 text-base" 
-            value={buy.buyCategory} 
-            onChange={handleChangeBuy}
+            value={team.teamCategory} 
+            onChange={handleChangeTeam}
           >
             <option disabled hidden value="">카테고리 선택</option>
-            <option value="1">배달음식</option>
-            <option value="2">생필품</option>
-            <option value="3">식료품</option>
-            <option value="4">가구/가전</option>
+            <option value="1">운동</option>
+            <option value="2">문화생활</option>
+            <option value="3">반려동물</option>
+            <option value="4">취미생활</option>
             <option value="5">기타</option>
           </select>
         </div>
         <div className="col-start-5 col-span-1">
           <label htmlFor="max" className="flex items-center text-sm font-medium text-gray-700 mb-1">
-            <img src={iconEdit} className="w-3 h-3"></img>&nbsp;모집인원
+            <img src={iconEdit} className="w-3 h-3" alt="edit"></img>&nbsp;모집인원
           </label>
-          <input type="number" name="max" id="max" value={buy.max} min="2" step="1" onInput={handleInputValidation}
-          onChange={handleChangeBuy} className="w-full h-9 pl-2 rounded-md border border-stone-400 text-base"/>
+          <input type="number" name="max" id="max" value={team.max} min="2" step="1" onInput={handleInputValidation}
+          onChange={handleChangeTeam} className="w-full h-9 pl-2 rounded-md border border-stone-400 text-base"/>
         </div>
         <div className="col-start-6 col-span-2">
           <label htmlFor="deadline" className="flex items-center text-sm font-medium text-gray-700 mb-1">
-            <img src={iconEdit} className="w-3 h-3"></img>&nbsp;공동구매 마감시간
+            <img src={iconEdit} className="w-3 h-3" alt="edit"></img>&nbsp;모집마감시간
           </label>
           <input type="datetime-local" required aria-required="true" name="deadline" id="deadline"
-          value={buy.deadline} onChange={handleChangeBuy} className="w-full h-9 rounded-md border border-stone-400 text-base"/>
+          value={team.deadline} onChange={handleChangeTeam} className="w-full h-9 rounded-md border border-stone-400 text-base"/>
         </div>
         <div className="col-start-2 col-span-6">
           <label htmlFor="title" className="flex items-center text-sm font-medium text-gray-700 mb-1">
-            <img src={iconEdit} className="w-3 h-3"></img>&nbsp;제목
+            <img src={iconEdit} className="w-3 h-3" alt="edit"></img>&nbsp;제목
           </label>
-          <input type="text" name="title" id="title" value={buy.title} onChange={handleChangeBuy} 
+          <input type="text" name="title" id="title" value={team.title} onChange={handleChangeTeam} 
           placeholder="제목을 입력하세요" className="w-full h-9 pl-2 rounded-md border border-stone-400 placeholder:text-base pb-1"/>
         </div>
         <div className="col-start-2 col-span-6">
           <label className="flex items-center text-sm font-medium text-gray-700 mb-1">
-            <img src={iconEdit} className="w-3 h-3"></img>&nbsp;거래 장소
+            <img src={iconEdit} className="w-3 h-3" alt="edit"></img>&nbsp;모임장소
           </label>
           <div className="flex">
             <div className="w-1/5 text-base">
@@ -201,15 +213,15 @@ const AddComponent = () => {
             </div>
             <div className="w-4/5 pl-1">
               <input className="w-full h-10 pl-2 rounded-md border border-stone-400 placeholder:text-base pb-1" name="addr" type="text"
-              placeholder="주소(우편번호 및 도로명 검색)" value={buy.location}/>
+              placeholder="주소(우편번호 및 도로명 검색)" value={team.location}/>
             </div>
           </div>
         </div>
         <div className="col-start-2 col-span-6">
           <label htmlFor="content" className="flex items-center text-sm font-medium text-gray-700 mb-1">
-            <img src={iconEdit} className="w-3 h-3"></img>&nbsp;내용
+            <img src={iconEdit} className="w-3 h-3" alt="edit"></img>&nbsp;내용
           </label>
-          <textarea name="content" id="content" value={buy.content} rows="6" onChange={handleChangeBuy} placeholder="내용을 입력하세요"
+          <textarea name="content" id="content" value={team.content} rows="6" onChange={handleChangeTeam} placeholder="내용을 입력하세요"
           className="w-full pl-2 rounded-md border border-stone-400 placeholder:text-base pb-1"></textarea>
         </div>
         <div className="col-start-6 col-span-2 mb-3">
