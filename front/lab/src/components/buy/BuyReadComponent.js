3967b50572d2b9b61a54b7d968fb2d44cd568d51
whitejh <56130599+whitejh@users.noreply.github.com>
2024-06-11 12:02:16 +0900

    [FE] Modify : 공동구매 목록,상세페이지 수정, 동네모임/동네장터에 수정기능 적용

--- front/lab/src/components/buy/BuyReadComponent.js
+++ front/lab/src/components/buy/BuyReadComponent.js
@@ -4,11 +4,14 @@ import useCustomMove from '../../hooks/useCustomMove';
 import MapComponent from '../common/MapComponent';
 import LandingComponent from '../common/mapSearch/LandingComponent';
 import ModalComponent from '../common/ModalComponent';
+import { modifyUser, getUser } from './../../api/userApi';
+import { useSelector } from 'react-redux';
 
 const initState = {
   buyNo: 0,
   title: '',
   location: '',
+  nickname: '',
   content: '',
   buyCategory: '',
   max: 0,
@@ -54,9 +57,7 @@ const BuyReadComponent = ({ buyNo }) => {
   }, [buy.deadline]);
   return (
     <>
-    
       <div className="detail-container">
-        
         <div>
           <div className="flex justify-between">
             <div className="my-5 mx-5">{recruit ? <span>모집 종료</span> : <span>모집 중</span>}</div>
--- front/lab/src/pages/market/ListPage.js
+++ front/lab/src/pages/market/ListPage.js
@@ -1,32 +1,70 @@
 import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 import ListComponent from '../../components/market/ListComponent';
-import SearchComponent from '../../components/common/SearchComponent';
 import LocationComponent from '../../components/common/location/LocationComponent';
+//import MoveMarkerComponent from '../../components/common/location/MoveLocationComponent';
+import SearchComponent from '../../components/common/SearchComponent';
 import SelectComponent from '../../components/common/SelectComponent';
+import useCustomMove from './../../hooks/useCustomMove';
+import ResultModal from '../../components/common/ResultModal';
 
 const ListPage = () => {
-  const [search, setsearch] = useState('');
-  const [sort, setsort] = useState('');
+  const [sort, setSort] = useState('');
+  const [search, setSearch] = useState('');
+  const { moveToAdd } = useCustomMove();
+  const [addResultModal, setAddResultModal] = useState(null);
+  const navigate = useNavigate();
+  const loginInfo = useSelector((state) => state.loginSlice); // 전역상태에서 loginSlice는 로그인 사용자의 상태정보
+
+  // ? 사용: loginInfo가 null/undefined일때도 오류없이 처리
+  const email = loginInfo?.email;
+
+  const handleClickWrite = (e) => {
+    if (!email) {
+      setAddResultModal('로그인 후 이용할 수 있습니다');
+    } else {
+      moveToAdd();
+    }
+  };
+
+  const handleModalClose = () => {
+    setAddResultModal(null);
+    navigate('/user/login'); // 로그인 페이지로 이동
+  };
 
   const handleSearch = (query) => {
-    setsearch(query);
+    setSearch(query);
   };
 
   const handleSort = (query) => {
-    setsort(query);
+    setSort(query);
   };
 
   return (
     <>
-      <div className="m-auto w-2/5 pb-2 text-3xl font-Jua">동네장터</div>
-      <div className="m-auto w-2/5 items-center">
+      <div className="m-auto w-1/2 pb-2 text-3xl font-Jua">동네모임</div>
+      <div className="m-auto w-1/2 items-center">
         <LocationComponent />
+        {/* <MoveMarkerComponent /> */}
       </div>
-      <div className="m-auto bg-slate-200 w-2/5 rounded-md px-10 py-4">
-        <SearchComponent onSearch={handleSearch}/>
-        <SelectComponent onSort={handleSort}/>
+      <div className="m-auto bg-slate-200 w-1/2 rounded-md px-10 py-4">
+        <SearchComponent onSearch={handleSearch} />
+
+        <div className="flex">
+          <SelectComponent onSort={handleSort} />
+          <button
+            type="button"
+            className="w-32 mb-2 float-right inline-block rounded bg-teal-400 px-6 pb-2 pt-2.5 text-base font-medium leading-normal text-white shadow-md transition duration-150 ease-in-out hover:bg-teal-500 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-teal-600 motion-reduce:transition-none"
+            onClick={handleClickWrite}
+          >
+            글쓰기
+          </button>
+        </div>
+
         <ListComponent search={search} sort={sort} />
       </div>
+      {addResultModal && <ResultModal title={'알림'} content={addResultModal} callbackFn={handleModalClose} />}
     </>
   );
 };
