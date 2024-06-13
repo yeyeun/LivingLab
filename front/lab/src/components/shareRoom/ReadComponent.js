2feaebdb052eddbda816f575a65b4d84cdc0a6ac
Wu Sang <a4042718@gmail.com>
2024-06-13 22:15:56 +0900

    merge전 백업용 커밋

--- front/lab/src/components/shareRoom/ReadComponent.js
+++ front/lab/src/components/shareRoom/ReadComponent.js
@@ -1,5 +1,5 @@
 
-import { useEffect, useState } from "react"
+import React,{ useEffect, useState } from "react"
 import { API_SERVER_HOST, getOne, deleteOne } from "../../api/shareRoomApi"
 import useRoomCustomMove from "../../hooks/useRoomCustomMove"
 import { useSelector } from 'react-redux';
@@ -19,7 +19,6 @@ const initState = {
 const ReadComponent = ({ roomNo }) => {
     const [shareRoom, setShareRoom] = useState(initState)
     const { moveToModify, moveToList } = useRoomCustomMove()
-    const [serverData, setServerData] = useState(initState);
     const [result, setResult] = useState(null)
     const loginState = useSelector((state) => state.loginSlice);
 
@@ -29,6 +28,8 @@ const ReadComponent = ({ roomNo }) => {
             setShareRoom(data)
         })
     }, [roomNo])
+
+
 
     const handleClickDelete = () => {
 
@@ -41,65 +42,27 @@ const ReadComponent = ({ roomNo }) => {
     }
 
     return (
-        <div class="flex flex-col w-3/4 gap-5 p-2 mx-auto bg-white shadow-lg select-none sm:p-4 sm:h-96 rounded-2xl sm:flex-row ">
-            {/* <img alt="..." src={`${host}/api/shareRoom/display/${shareRoom.uploadFileNames[0]}`} class="h-52 sm:h-80 sm:w-72 rounded-xl"/>            */}
-            {shareRoom.uploadFileNames.map((imgFile, i) =>
-                <img
-                    alt="tip"
-                    key={i}
-                    width={600}
-                    src={`${host}/api/shareRoom/display/${imgFile}`}
-                    className="h-52 sm:h-80 sm:w-72 rounded-xl" />
-            )}
-            <div class="flex flex-col flex-1 gap-5 sm:p-2">
-                <div class="flex flex-col flex-1 gap-5">
-                    <div class="w-full bg-gray-200 h-10 rounded-2xl flex items-center justify-center">{shareRoom.title}
-                    </div>
-                    <div class="w-full h-52 bg-gray-200 rounded-2xl px-4">
-                        <div class="flow-root">
-                            <dl class="-my-3 divide-y divide-gray-100 text-sm">
-                                <div class="pt-6 pb-3 pl-2 flex">
-                                    <div class="font-bold text-gray-900 flex-none text-lg">월세</div>
-                                    <div class="grow"></div>
-                                    <div class="text-gray-700 sm:col-span-2 flex-none">{shareRoom.rentFee}</div>
+
+        <div id="wrap" className="pt-10 w-full text-center mx-auto bg-yellow-200 ">
+            <div id="images" className="w-[1200px] p-2.5 mx-auto">
+                <div id="grid" className="grid grid-cols-custom grid-rows-2 gap-2 w-full h-[440px]">
+                          {shareRoom.uploadFileNames.map((imgFile, index) => (
+                        <React.Fragment key={index}>
+                            {index === 0 ? (
+                                <div id={`child-first-${index}`} className="row-span-2 relative overflow-hidden">
+                                    <img src={`${host}/api/shareRoom/display/${imgFile}`} className="position-absolute object-cover w-full h-full"></img>
                                 </div>
-                                <div class="py-3 pl-2 flex">
-                                    <div class="font-bold text-gray-900 flex-none text-lg">주차</div>
-                                    <div class="grow"></div>
-                                    <div class="text-gray-700 sm:col-span-2 flex-none">{shareRoom.parking}</div>
-                                </div>
-                                <div class="py-3 pl-2 flex">
-                                    <div class="font-bold text-gray-900 flex-none text-lg">옵션</div>
-                                    <div class="grow"></div>
-                                    <div class="text-gray-700 sm:col-span-2 flex-none">{shareRoom.option1}</div>
-                                </div>
-                                <div class="py-3 pl-2 flex">
-                                    <div class="font-bold text-gray-900 flex-none text-lg">위치</div>
-                                    <div class="grow"></div>
-                                    <div class="text-gray-700 sm:col-span-2 flex-none">{shareRoom.location}</div>
-                                </div>
-                            </dl>
-                        </div>
-                    </div>
-                    <div class="flex gap-3 mt-auto">
-                        <div class="w-20 h-8 ml-auto bg-gray-200 rounded-full">
-                            문의하기
-                        </div>
-                    </div>
-                    <div className="flex justify-end p-4">
-                        {loginState.id === shareRoom.userId && (
-                            <>
-                                <button type="button" className="rounded p-4 m-2 text-xl w-32 text-white bg-gray-400" onClick={() => moveToModify(roomNo)}>
-                                    Modify
-                                </button>
-                                <button type="button" className="rounded p-4 m-2 text-xl w-32 text-white bg-red-400" onClick={handleClickDelete}>
-                                    Delete
-                                </button>
-                            </>
-                        )}
-                    </div>
+                            ) : (
+                                <>
+                                    <div id={`child-${index}`} className="relative overflow-hidden">
+                                        <img src={`${host}/api/shareRoom/display/${imgFile}`} className="position-absolute object-cover w-full h-full"></img>
+                                    </div>
+                                </>
+                            )}
+                        </React.Fragment>
+                    ))}
                 </div>
-            </div>
+            </div> 
         </div>
     );
 }
--- front/lab/tailwind.config.js
+++ front/lab/tailwind.config.js
@@ -10,7 +10,10 @@ module.exports = {
       colors : {
         'mainColor' : '#6AC5B7',
         'subColor' : '#89540A'
-      }
+      },
+      gridTemplateColumns: {
+        'custom': '586px minmax(0px, 1fr) minmax(0px, 1fr)'
+      },
     },
   },
   plugins: [],
