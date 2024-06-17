b8d715a496e4a4be4f5dfacb2c66f170db39315a
lwjddns <jwlee20541@gmail.com>
2024-06-17 15:56:46 +0900

    git commit -m "Deleted all files and directories"

--- front/lab/src/App.js
+++
@@ -1,8 +0,0 @@
-import './App.css';
-import { RouterProvider } from 'react-router-dom';
-import root from './router/root';
-
-function App() {
-  return <RouterProvider router={root} />;
-}
-export default App;
