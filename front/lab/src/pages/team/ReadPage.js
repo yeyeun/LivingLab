de27cee9afcb742270367f87defb70680267bebd
whitejh <56130599+whitejh@users.noreply.github.com>
2024-06-17 14:57:57 +0900

    [FE] : Modify : 동네모임 상세페이지 속 참여인원 목록 컴포넌트 수정

--- front/lab/src/pages/team/ReadPage.js
+++ front/lab/src/pages/team/ReadPage.js
@@ -1,12 +1,17 @@
 import { useParams } from 'react-router-dom';
 import ReadComponent from '../../components/team/ReadComponent';
+import PartComponent from '../../components/team/PartComponent';
 
 const ReadPage = () => {
   const { teamNo } = useParams();
   return (
-    <div>
-      <ReadComponent teamNo={teamNo} />
-    </div>
+    <>
+      <div className="flex justify-center">
+        <ReadComponent teamNo={teamNo} />
+        <PartComponent teamNo={teamNo} />
+      </div>
+      <PartComponent teamNo={teamNo} />
+    </>
   );
 };
 
