96a8509cae73397d802bc87bdba6e4d8a16353db
Wu Sang <a4042718@gmail.com>
2024-06-21 13:48:37 +0900

    [BE,FE] Feat : 호버기능추가

--- front/lab/tailwind.config.js
+++ front/lab/tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ['./src/**/*.{js, jsx, ts, tsx}'],
   theme: {
+    screens: {
+      xs: '320px',
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
     extend: {
       fontFamily: {
         NotoSansKR: ['Noto Sans KR', 'sans-serif'],
@@ -17,6 +24,28 @@ module.exports = {
       spacing: {
         2.5: '0.625rem', // 10px
       },
+      keyframes: {
+        spinIn: {
+          to: { transform: 'rotate(18deg)'}
+        },
+        spinOut: {
+          to: { transform: 'rotate(360deg)'}
+        }
+      },
+      animation: {
+        spinIn: 'spinIn 30s linear infinite',
+        spinout: 'spinIn 10s linear infinite',
+      },
+      backdropFilter: ['hover', 'focus'],
+      filter: { // 필터 유틸리티 활성화
+        'none': 'none',
+        'blur': 'blur(20px)',
+      },
+      transitionProperty: { // 트랜지션 속성 활성화
+        'height': 'height',
+        'spacing': 'margin, padding',
+        'blur': 'filter',
+      }
     },
   },
   plugins: [],
