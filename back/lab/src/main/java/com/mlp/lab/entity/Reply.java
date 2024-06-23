dd00f6b6e94f435524708ecce2ff98ae609e34c1
young eun <woyelove@gmail.com>
2024-06-24 00:55:08 +0900

    [BE] Feat : 댓글 기능 구현 중

---
+++ back/lab/src/main/java/com/mlp/lab/entity/Reply.java
@@ -0,0 +1,27 @@
+package com.mlp.lab.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "reply")
+public class Reply {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long replyNo;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "community_commNo")
+    private Community community;
+}
