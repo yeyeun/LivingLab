5d5a1be6b8dfbc0508c79669f45a252f5fb504e4
young eun <woyelove@gmail.com>
2024-07-02 09:47:51 +0900

    배포용으로 수정

---
+++ back/lab/src/main/java/com/mlp/lab/controller/WebErrorController.java
@@ -0,0 +1,13 @@
+package com.mlp.lab.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebErrorController implements ErrorController {
+    @GetMapping({"/", "/error"})
+    public String index() {
+        return "index.html";
+    }
+}
